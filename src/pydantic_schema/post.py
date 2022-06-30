d4394e1642baa82f2f99d792dc0796f728379c6d
Vladiid <vladiid84@gmail.com>
2022-06-30 15:53:56 +0300

    change @validator func

--- src/pydantic_schema/post.py
+++ src/pydantic_schema/post.py
@@ -6,7 +6,7 @@ class Post(BaseModel):
 
     @validator("bookingid")
     def check(cls, v):
-        if v > 4000:
+        if v > 2000:
             raise ValueError('Is not less than 2200')
         else:
             return v
