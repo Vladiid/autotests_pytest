c9b7420af0132ca6ad1bee47a02e12415fe89654
Vladiid <vladiid84@gmail.com>
2022-07-08 22:10:49 +0300

    new_test

--- configuration.py
+++ configuration.py
@@ -1,2 +1,3 @@
-SERVICE_URL = "https://restful-booker.herokuapp.com/booking"
-SERVICE_URL_R = "http://numbersapi.com/random/year?json"
+SERVICE_URL = "https://gorest.co.in/public/v1/users"
+# SERVICE_URL = "https://restful-booker.herokuapp.com/booking"
+# SERVICE_URL_R = "http://numbersapi.com/random/year?json"
