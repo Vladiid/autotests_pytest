70b50cd8718b5b3569a1ed137b88ef65524f9a10
Vladiid <vladiid84@gmail.com>
2022-07-14 19:03:26 +0300

    new_validator

---
+++ src/enums/user_enums.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+class Genders(Enum):
+    female = 'female'
+    male = 'male'
+
+
+class Statuses(Enum):
+    active = 'active'
+    inactive = 'inactive'
+
+
+class UserErrors(Enum):
+    WRONG_EMAIL = "Email doesn't contain @"
+
+
+
