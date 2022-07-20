5921db61700381cbed7375f1701da76d6ccefe41
Vladiid <vladiid84@gmail.com>
2022-07-20 18:14:16 +0300

    add_fixture

--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -1,4 +1,4 @@
-import requests
+import pytest
 
 from src.baseclasses.response import Response
 from src.schemas.user import User
@@ -6,8 +6,22 @@ from src.schemas.user import User
 
 def test_getting_users_list(get_users, make_number):
     Response(get_users).assert_status_code(200).validate(User)
-    print(make_number)
+    # print(make_number)
 
 
+@pytest.mark.prod
+@pytest.mark.skip
 def test_another():
     assert 1 == 1
+
+
+@pytest.mark.dev
+@pytest.mark.parametrize('first_value, second_value, result', [
+    (1, 2, 3),
+    (-1, -2, -3),
+    (-1, 2, 1),
+    ('b', -2, None),
+    ('b', 'b', None),
+])
+def test_calculator(first_value, second_value, result, calculate):
+    assert calculate(first_value, second_value) == result
